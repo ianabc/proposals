d1af6624c83c97a358d5b09ddacb9e850628c3d8
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-08-20 18:07:47 +0500

    Added numbers in form fields

--- app/controllers/proposal_fields_controller.rb
+++ app/controllers/proposal_fields_controller.rb
@@ -16,18 +16,22 @@ class ProposalFieldsController < ApplicationController
     if %w[Date Radio Text SingleChoice MultiChoice PreferredImpossibleDate].include?(params[:type])
       @fieldable = "ProposalFields::#{params[:type]}".safe_constantize.new(date_field_params)
     end
-    @proposal_field = @proposal_form.proposal_fields.new(proposal_field_params)
-    @proposal_field.fieldable = @fieldable
-    if @proposal_field.insert_at(@proposal_field.position)
-      @proposal_form.update(updated_by: current_user)
-      redirect_to edit_proposal_type_proposal_form_url(@proposal_form.proposal_type, @proposal_form),
-                  notice: "Field was successfully created."
+    get_positions
+    if check_position?
+      @proposal_field = @proposal_form.proposal_fields.new(proposal_field_params)
+      @proposal_field.fieldable = @fieldable
+      if @proposal_field.insert_at(@proposal_field.position)
+        @proposal_form.update(updated_by: current_user)
+        redirect_to edit_proposal_type_proposal_form_url(@proposal_form.proposal_type, @proposal_form),
+                    notice: "Field was successfully created."
+      else
+        redirect_to edit_proposal_type_proposal_form_url(@proposal_form.proposal_type, @proposal_form),
+                    alert: @proposal_form.errors
+      end
     else
-      redirect_to edit_proposal_type_proposal_form_url(@proposal_form.proposal_type, @proposal_form),
-                  alert: @proposal_form.errors
+      redirect
     end
   end
-
 
   def edit
     render partial: 'proposal_fields/fields_form',
@@ -35,13 +39,18 @@ class ProposalFieldsController < ApplicationController
   end
 
   def update
-    if @proposal_field.update(proposal_field_params) && @proposal_field.fieldable.update(date_field_params)
-      @proposal_form.update(updated_by: current_user)
-      redirect_to edit_proposal_type_proposal_form_url(@proposal_form.proposal_type, @proposal_form),
-                  notice: "Field was successfully updated."
+    get_positions
+    if check_position?
+      if @proposal_field.update(proposal_field_params) && @proposal_field.fieldable.update(date_field_params)
+        @proposal_form.update(updated_by: current_user)
+        redirect_to edit_proposal_type_proposal_form_url(@proposal_form.proposal_type, @proposal_form),
+                    notice: "Field was successfully updated."
+      else
+        redirect_to edit_proposal_type_proposal_form_url(@proposal_form.proposal_type, @proposal_form),
+                    alert: @proposal_form.errors
+      end
     else
-      redirect_to edit_proposal_type_proposal_form_url(@proposal_form.proposal_type, @proposal_form),
-                  alert: @proposal_form.errors
+      redirect
     end
   end
 
@@ -67,5 +76,25 @@ class ProposalFieldsController < ApplicationController
 
     param.permit(:preferred_dates_1, :preferred_dates_2, :preferred_dates_3, :preferred_dates_4,
                  :preferred_dates_5, :impossible_dates_1, :impossible_dates_2)
+  end
+
+  def get_positions
+    @position = @proposal_form.highest_field_position
+    @field = params[:proposal_field]
+    @field_position = @field[:position].to_i
+  end
+
+  def check_position?
+    ((@position + 1) == @field_position) || (@field_position.positive? && @field_position <= @position)
+  end
+
+  def redirect
+    if (@position + 1) == 1
+      redirect_to edit_proposal_type_proposal_form_url(@proposal_form.proposal_type, @proposal_form),
+                  alert: "Postion should be greater than 0"
+    else
+      redirect_to edit_proposal_type_proposal_form_url(@proposal_form.proposal_type, @proposal_form),
+                  alert: "Postion should be greater than 0 and smaller or equal to #{@position + 1}"
+    end
   end
 end
