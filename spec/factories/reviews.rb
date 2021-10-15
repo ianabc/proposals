d8d6af2ce50de6ab5712655837bce7762d4658cd
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-10-15 17:24:38 +0500

    Fixed some code error

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :review do
-
     association :proposal, factory: :proposal, strategy: :create
     association :person, factory: :person, strategy: :create
   end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-
   describe 'associations' do
     it { should belong_to(:proposal) }
     it { should belong_to(:person) }
