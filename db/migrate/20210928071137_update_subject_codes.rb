009baea898e84411f92677fde25f5a8657036c1c
Rizwan Ali <devali.rizwan@gmail.com>
2021-10-19 14:38:05 +0500

    Fix migration

--- db/migrate/20210928071137_update_subject_codes.rb
+++ db/migrate/20210928071137_update_subject_codes.rb
@@ -1,11 +1,15 @@
 class UpdateSubjectCodes < ActiveRecord::Migration[6.1]
   def change
+    subject_category = SubjectCategory.first
+
+    subject_category = SubjectCategory.create!(name: 'None', code: 'none') unless subject_category
+
     subject = Subject.find_by(title: 'Arithmetic Number Theory')
 
     if subject
       subject.update(code: 'ARNT')
     else
-      Subject.find_or_create_by(code: 'ARNT', title: 'Arithmetic Number Theory')
+      Subject.find_or_create_by(code: 'ARNT', title: 'Arithmetic Number Theory', subject_category_id: subject_category.id)
     end
   end
 end
