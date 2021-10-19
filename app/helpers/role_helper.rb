5887fcebacbae9a9da53da8070b3229ea1248725
Rizwan Ali <devali.rizwan@gmail.com>
2021-10-19 16:57:11 +0500

    Added access control for reviews

--- app/helpers/role_helper.rb
+++ app/helpers/role_helper.rb
@@ -2,4 +2,10 @@ module RoleHelper
   def users_without_role(role)
     User.where.not(id: role.users.ids)
   end
+
+  def check_review_privilege
+    return true if can? :manage, Review
+
+    false
+  end
 end
