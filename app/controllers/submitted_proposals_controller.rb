fee639bd329fc34b142bf44f15214d2034afeee7
Rizwan Ali <devali.rizwan@gmail.com>
2021-10-26 16:25:41 +0500

    Fixed review book object error

--- app/controllers/submitted_proposals_controller.rb
+++ app/controllers/submitted_proposals_controller.rb
@@ -479,6 +479,8 @@ class SubmittedProposalsController < ApplicationController
   def review_file(review)
     @review_files = []
     review["reports"]&.each do |report|
+      next unless report["fileID"].present?
+
       review_file_url(report["fileID"])
       @review_files << report["fileID"]
     end
