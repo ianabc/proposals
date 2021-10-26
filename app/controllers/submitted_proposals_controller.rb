f40401bfd347a22b4a02579ce869b48c07e15240
Rizwan Ali <devali.rizwan@gmail.com>
2021-10-26 17:19:45 +0500

    Fixed some code errors

--- app/controllers/submitted_proposals_controller.rb
+++ app/controllers/submitted_proposals_controller.rb
@@ -479,7 +479,7 @@ class SubmittedProposalsController < ApplicationController
   def review_file(review)
     @review_files = []
     review["reports"]&.each do |report|
-      next unless report["fileID"].present?
+      next if report["fileID"].blank?
 
       review_file_url(report["fileID"])
       @review_files << report["fileID"]
