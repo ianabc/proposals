10d610063e1ac60626f23b1340be26cb45a6ed51
Rizwan Ali <devali.rizwan@gmail.com>
2021-11-10 13:46:06 +0500

    Fix codacy

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -72,7 +72,6 @@ module Users
       end
     end
 
-
     # If you have extra params to permit, append them to the sanitizer.
     # def configure_account_update_params
     #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
