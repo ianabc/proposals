fd5b8dbf5a0e2dd995445530cba9181cb3bf4742
Rizwan Ali <devali.rizwan@gmail.com>
2021-11-24 19:53:12 +0500

    Added validation for start and end date

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -7,10 +7,20 @@ export default class extends Controller {
   excludeDatesList() {
     let startDate = this.startDateTarget.value
     let endDate = this.endDateTarget.value
-    if (startDate === "" || endDate === "") {
+    var start = new Date(startDate)
+    var end = new Date(endDate)
+    if(startDate !== "" && start.getDay() !== 0) {
+      toastr.error("Start Date must be Sunday!")
+      document.getElementById('location_exclude_dates').length = 0;
+    }
+    if(endDate !== "" && end.getDay() !== 5){
+      toastr.error("End Date must be Friday!")
+      document.getElementById('location_exclude_dates').length = 0;
+    }
+    else if (startDate === "" || endDate === "") {
       return
     }
-    else {
+    else if(start.getDay() === 0 && end.getDay() === 5) {
       let _this = this
       $.post(`/locations/weeks_exclude_dates?start=${startDate}&end=${endDate}`, function(data) {
         const selectBox = _this.excludeDatesTarget;
