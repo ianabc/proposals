2688a7137914c538a41dba80e5a72b32cdbfc904
Brent <brentk@birs.ca>
2021-12-09 10:05:11 -0700

    refactor

--- app/jobs/export_scheduled_proposals_job.rb
+++ app/jobs/export_scheduled_proposals_job.rb
@@ -3,24 +3,27 @@ class ExportScheduledProposalsJob < ApplicationJob
 
   def workshops_api
     url = ENV['WORKSHOPS_API_URL']
-    Rails.logger.info("WORKSHOPS_API_URL not set!") and return if url.blank?
-
+    Rails.logger.info("WORKSHOPS_API_URL not set!") if url.blank?
     url
+  end
+  
+  def publish_proposal(proposal)
+    request_body = ScheduledProposalService.new(proposal).event
+    response = RestClient.post url, request_body.to_json, content_type: :json, accept: :json
+    Rails.logger.info("Posted proposal #{code} to Workshops. Response: #{response.body}")
+  rescue => e
+    Rails.logger.info("Error posting proposal #{code} to Workshops: #{e}. Reponse: #{response.body}")
   end
 
   def perform(proposal_codes)
-    url = workshops_api || return
+    url = workshops_api
+    return if url.blank?
 
     proposal_codes.each do |code|
       proposal = Proposal.find(code)
       next if proposal.blank?
 
-      request_body = ScheduledProposalService.new(proposal).event
-
-      response = RestClient.post url, request_body.to_json, content_type: :json, accept: :json
-      Rails.logger.info("Posted proposal #{code} to Workshops. Response: #{response.body}")
-    rescue => e
-      Rails.logger.info("Error posting proposal #{code} to Workshops: #{e}. Reponse: #{response.body}")
+      publish_proposal(proposal)
     end
   end
 end
