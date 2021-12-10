2022c1cdbbda618f289fb4be7ef0e155fd7e4b41
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-12-10 12:02:49 +0500

    Fixed weeks number bug and uneditable

--- app/models/location.rb
+++ app/models/location.rb
@@ -20,15 +20,7 @@ class Location < ApplicationRecord
   def num_weeks
     return 0 if start_date.blank? || end_date.blank?
 
-    weeks = (end_date.to_time - start_date.to_time).seconds.in_weeks.to_i.abs
-    return weeks if exclude_dates.blank?
-
-    remove_excluded_dates(weeks)
-  end
-
-  def remove_excluded_dates(weeks)
-    exclude_dates.delete("")
-    weeks - exclude_dates.count
+    (end_date.to_time - start_date.to_time).seconds.in_weeks.to_i.abs
   end
 
   def excluded_dates
@@ -63,7 +55,6 @@ class Location < ApplicationRecord
     date = Date.parse(ds)
     errors.add(field, "#{ds} must be after Start Date") if date < start_date
     errors.add(field, "#{ds} must be before End Date") if date > end_date
-    
   rescue Date::Error
     errors.add(field, "#{ds} is not a valid date string.")
   end
