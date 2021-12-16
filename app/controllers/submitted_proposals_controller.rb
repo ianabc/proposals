5ee336c1ff42993abbd5104ee5b0285a587cafc9
Rizwan Ali <devali.rizwan@gmail.com>
2021-12-16 19:40:00 +0500

    Fix

--- app/controllers/submitted_proposals_controller.rb
+++ app/controllers/submitted_proposals_controller.rb
@@ -438,7 +438,7 @@ class SubmittedProposalsController < ApplicationController
 
   def send_email_proposals
     add_files
-    organizers_email = @proposal.invites.where(invited_as: 'Organizer')&.pluck(:email)
+    organizers_email = @proposal.invites.where(invited_as: 'Organizer', status: :confirmed)&.pluck(:email)
     @email.new_email_organizers(organizers_email) if @email.save
     @errors << @email.errors.full_messages unless @email.errors.empty?
   end
