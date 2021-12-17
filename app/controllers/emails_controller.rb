70ceb88d777543286b17d8d3bb5476933182b570
Rizwan <devali.rizwan@gmail.com>
2021-12-17 17:28:42 +0500

    Reorder lead and organizers email

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -46,6 +46,6 @@ class EmailsController < ApplicationController
   def set_organizer_emails
     proposals = Proposal.where(id: params[:ids])
     organizer_emails = Invite.where(proposal_id: params[:ids], invited_as: 'Organizer', status: :confirmed).pluck(:email)
-    @organizer_emails = organizer_emails + proposals.map(&:lead_organizer).pluck(:email)
+    @organizer_emails = proposals.map(&:lead_organizer).pluck(:email) + organizer_emails
   end
 end
