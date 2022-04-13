559aa4a7155c5d265d701007a5ab53cefa699edc
Hassan Mahmood <devhassan.mahmood@gmail.com>
2022-04-13 17:05:56 +0500

    fixed error

--- app/controllers/schedule_runs_controller.rb
+++ app/controllers/schedule_runs_controller.rb
@@ -2,7 +2,7 @@ class ScheduleRunsController < ApplicationController
   def destroy
     @schedule_run = ScheduleRun.find(params[:id])
     @schedule_run.destroy
-    flash[:notice]= "Schedule result is deleted successfully"
+    flash[:notice] = "Schedule result is deleted successfully"
     redirect_to new_schedule_path
   end
 end
