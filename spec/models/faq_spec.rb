27a9258d35fd14ab58487829eee3f8ca134021ec
Hassan Mahmood <devhassan.mahmood@gmail.com>
2022-04-18 15:25:41 +0500

    update code styling issues

--- spec/models/faq_spec.rb
+++ spec/models/faq_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Faq, type: :model do
   it 'has valid factory' do
     expect(build(:faq)).to be_valid
   end
-  
+
   it 'requires a question' do
     faq = build(:faq, question: '')
     expect(faq.valid?).to be_falsey
