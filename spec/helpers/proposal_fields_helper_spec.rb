b5be60766d1468cb2fd39e4bd441407f9e077e6d
Hassan Mahmood <devhassan.mahmood@gmail.com>
2022-04-19 17:27:16 +0500

    Added rspecs for coverage

--- spec/helpers/proposal_fields_helper_spec.rb
+++ spec/helpers/proposal_fields_helper_spec.rb
@@ -64,6 +64,33 @@ RSpec.describe ProposalFieldsHelper, type: :helper do
         answer
         expect(multichoice_answer(field, proposal)).to eq(nil)
       end
+
+      it 'returns when proposal is not present' do
+        answer
+        proposal = nil
+        expect(multichoice_answer(field, proposal)).to eq(nil)
+      end
+    end
+  end
+
+  describe "#multichoice_answer_with_version" do
+    let(:proposal) { create(:proposal) }
+    let(:proposal) { create(:proposal) }
+    let(:field) { create(:proposal_field, :multi_choice_field) }
+    let!(:version) { create(:proposal_version, proposal_id: proposal.id) }
+
+    context 'when multichoice filed has no answer' do
+      let!(:answer) { build(:answer, proposal: proposal, proposal_field: field, answer: '') }
+
+      it 'returns nil' do
+        expect(multichoice_answer_with_version(field, proposal, version)).to eq(nil)
+      end
+
+      it 'returns when proposal is not present' do
+        answer
+        proposal = nil
+        expect(multichoice_answer_with_version(field, proposal, version)).to eq(nil)
+      end
     end
   end
 
@@ -121,6 +148,68 @@ RSpec.describe ProposalFieldsHelper, type: :helper do
       it 'It should return an anser containing proposal and field' do
         expect(answer(field, proposal)).not_to be_present
       end
+    end
+
+    context '#answer_with_version' do
+      let(:version) { create(:proposal_version, proposal_id: proposal.id) }
+      let!(:answer_obj) { create(:answer, proposal: proposal, proposal_field: field) }
+
+      it 'It should return because proposal is not present' do
+        proposal = nil
+        expect(answer_with_version(field, proposal, version)).not_to be_present
+      end
+
+      it 'It should not return from first line because proposal is present' do
+        expect(answer_with_version(field, proposal, version)).not_to be_present
+      end
+    end
+  end
+
+  describe '#tab_errors' do
+    let(:locations) { create_list(:location, 4) }
+    let!(:proposal_type) { create(:proposal_type, locations: locations) }
+    let!(:proposal) { create(:proposal, proposal_type: proposal_type) }
+    it 'when it returns two' do
+      param_tab = 'tab-2'
+      response = tab_errors(proposal, param_tab)
+      expect(response).to be_a String
+      expect(response).to eq 'two'
+    end
+    it 'when it returns one without params passing' do
+      param_tab = 'test'
+      response = tab_errors(proposal, param_tab)
+      expect(response).to be_a String
+      expect(response).to eq 'one'
+    end
+  end
+
+  describe '#tab_one' do
+    let(:locations) { create_list(:location, 4) }
+    let!(:proposal_type) { create(:proposal_type, locations: locations) }
+    let!(:proposal) { create(:proposal, proposal_type: proposal_type) }
+    let(:invite) { create(:invite, invited_as: "Organizer", proposal_id: proposal.id) }
+    it 'when it returns true' do
+      expect(tab_one(proposal)).to eq true
+    end
+  end
+
+  describe '#tab_two' do
+    let!(:locations) { create_list(:location, 4) }
+    let!(:proposal_type) { create(:proposal_type, locations: locations) }
+    let!(:proposal) { create(:proposal, proposal_type: proposal_type) }
+    let!(:proposal_form) { create(:proposal_form, status: :active) }
+    let!(:proposal_field) { create(:proposal_field, :radio_field, :location_based, proposal_form: proposal_form) }
+    it 'when it returns false and does not enter in loop' do
+      expect(tab_two(proposal)).to eq false
+    end
+
+    it '#tab_three when it returns true with empty locations and does not enter in loop' do
+      expect(tab_three(proposal)).to eq true
+    end
+
+    it '#tab_three does not enter in loop and returns false' do
+      proposal.update(location_ids: [locations.first.id])
+      expect(tab_three(proposal)).to eq false
     end
   end
 end
