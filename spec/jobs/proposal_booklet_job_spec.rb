c5857c77cb9bfdabe3fb31589375cb51d5a644d0
Hassan Mahmood <devhassan.mahmood@gmail.com>
2022-05-19 17:00:46 +0500

    Added rspec for job

--- spec/jobs/proposal_booklet_job_spec.rb
+++ spec/jobs/proposal_booklet_job_spec.rb
@@ -6,11 +6,20 @@ RSpec.describe ProposalBookletJob, type: :job do
     let(:proposals) { create_list(:proposal, 2) }
     let(:user) { create(:user) }
 
-    it "When no reviews are exported for proposals" do
+    it "When booklet is created for multiple proposals" do
       proposals.first.update(subject_id: subject.first.id)
       proposals.last.update(subject_id: subject.last.id)
       proposals_ids = "#{proposals.first.id}, #{proposals.last.id}"
       counter = 2
+      table = 'toc'
+      response = ProposalBookletJob.perform_now(proposals_ids, table, counter, user)
+      expect(response).not_to be_present
+    end
+
+    it "When booklet is created for single proposals" do
+      proposals.first.update(subject_id: subject.first.id)
+      proposals_ids = "#{proposals.first.id}"
+      counter = 1
       table = 'toc'
       response = ProposalBookletJob.perform_now(proposals_ids, table, counter, user)
       expect(response).not_to be_present
