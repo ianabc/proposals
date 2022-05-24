294b92af3ece149c69b900b3c5cf1b0585791869
Hassan Mahmood <devhassan.mahmood@gmail.com>
2022-05-24 14:32:34 +0500

    updated fix for tinymce

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.digest = false
+
   # Add to settle tinymce issue
   config.assets.debug = true
 
