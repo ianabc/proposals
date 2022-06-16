3cdbed38c526dcc931267db4847aeaadbd76ecfc
Hassan Mahmood <devhassan.mahmood@gmail.com>
2022-06-16 13:26:14 +0500

    Invite Page refresh issues fixes

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -13,7 +13,7 @@ class Invite < ApplicationRecord
 
   validates :firstname, :lastname, :email, :invited_as,
             :deadline_date, presence: true
-  # validates_email_realness_of :email
+  validates :email, format: /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i
 
   validate :deadline_not_in_past, :proposal_title
   validate :one_invite_per_person, on: :create
