0a798847b688d9522f4d627cdc365088c332e093
Hassan Mahmood <devhassan.mahmood@gmail.com>
2022-09-07 23:42:59 +0500

    revert tempraray comit

--- app/controllers/submitted_proposals_controller.rb
+++ app/controllers/submitted_proposals_controller.rb
@@ -279,9 +279,9 @@ class SubmittedProposalsController < ApplicationController
     end
     return if flash[:alert].present?
 
-    response = RestClient.post 'https://ef.msp.org/api/birs/',
+    response = RestClient.post ENV.fetch('EDITFLOW_API_URL', nil),
                                { query: edit_flow_query, fileMain: File.open(@pdf_path) },
-                               { x_editflow_api_token: '4qHHBwqn6YMvHxRrmWFKtcF-h3hi9eyzlTOekEtf80E' }
+                               { x_editflow_api_token: ENV.fetch('EDITFLOW_API_TOKEN', nil) }
 
     query_response_body(response)
     Rails.logger.info { "\n\n*****************************************\n\n" }
