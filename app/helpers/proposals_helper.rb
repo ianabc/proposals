a49fc6206b90e0c2bfdfa5a592fb57bccdadebc0
Hassan Mahmood <devhassan.mahmood@gmail.com>
2022-09-08 18:26:18 +0500

    updated version status according to proposal

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -301,7 +301,11 @@ module ProposalsHelper
   end
 
   def proposal_version(version, proposal)
-    ProposalVersion.find_by(version: version, proposal_id: proposal.id)
+    @proposal_version = ProposalVersion.find_by(version: version, proposal_id: proposal.id)
+    if @proposal_version.status != proposal.status
+      @proposal_version.update(status: proposal.status)
+    end
+    return @proposal_version
   end
 
   def proposal_outcome
